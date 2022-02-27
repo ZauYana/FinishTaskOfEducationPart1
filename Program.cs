8707dac79fe6d94b86a775e49d933c6a2ddc6b15
ZauYana <zaulskayana@gmail.com>
2022-02-27 22:10:50 +0300

    add TheSolutionOfTask

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿string[] massivOfString = { "Сколько", "дурака", "не", "мотивируй", ",", "максимум", "чего", "вы", "добьетесь", "-", "это", "мотивированный", "дурак" };
+
+
+string[] CreateMassivOfStrings(string[] massivOfFirstText)
+{
+
+    string[] massivOfThreeOrLessLetters = new string[] { };
+    int j = 0;
+    for (int i = 0; i < massivOfFirstText.Length; i++)
+    {
+        if (massivOfFirstText[i].Length <= 3)
+        {
+
+            Array.Resize(ref massivOfThreeOrLessLetters, massivOfThreeOrLessLetters.Length + 1);
+            massivOfThreeOrLessLetters[massivOfThreeOrLessLetters.Length - 1] = massivOfFirstText[i];
+
+        }
+
+    }
+    return massivOfThreeOrLessLetters;
+}
+
+
+void PrintMassiv(string[] massivOfSomeText)
+{
+    for (int i = 0; i < massivOfSomeText.Length; i++)
+    {
+        Console.WriteLine(massivOfSomeText[i]);
+    }
+}
+
+Console.WriteLine ("Первоначальный массив:");
+PrintMassiv(massivOfString);
+Console.WriteLine();
+string[] massivOfLessOrThreeLetters = CreateMassivOfStrings(massivOfString);
+Console.WriteLine ("Массив со словами, состоящими не более, чем из трех букв:");
+PrintMassiv(massivOfLessOrThreeLetters);
